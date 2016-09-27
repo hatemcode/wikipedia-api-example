57a710df4b6e45377d954f8cbc030809ad3d538d
Hatem A <hatemcode@users.noreply.github.com>
2016-09-27 20:11:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/**
+ * Wikipedia API Example
+ * @author Hatem A. <hatem@tuta.io>
+ * @version 1.0
+ * @licence Licensed under the Apache License, Version 2.0
+ */
+
 function close(){
   $("#query-input").val("");
 }
